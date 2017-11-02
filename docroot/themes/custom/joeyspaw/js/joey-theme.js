a0f0dbf0adb48b2f9cb8a12cab06287e050d6d59
Gregg Shanefelt <gshanefelt@beyondspotsanddots.com>
2017-11-02 11:09:07 -0400

    Font awesome and stuff

--- docroot/themes/custom/joeyspaw/js/joey-theme.js
+++ docroot/themes/custom/joeyspaw/js/joey-theme.js
@@ -9,6 +9,23 @@
         $("#ui-id-2").replaceWith("<span>Photo Gallery</span>");
         $("#ui-id-1").replaceWith("<span>Photo Gallery</span>");
 
+        /***** PARALLAX BG IMAGE SCROLL *****/
+
+        var parallax = $('.pre-content'),
+          speed = 0.35;
+
+        $(window).on("resize scroll",function(e) {
+          var isMobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || window.innerWidth < 980;
+
+          if (!isMobile) {
+            var windowYOffset = window.pageYOffset,
+              backgroundPos = "50% " + (windowYOffset * speed) + "px";
+
+            parallax.css('backgroundPosition', backgroundPos);
+          }
+        });
+
+
       }
     }
   };
