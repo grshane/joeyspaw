28ba46e139b3f9e0647e3c48a254a33b7131907b
David Trainer <david@minimalmedia.com>
2017-11-21 16:43:14 -0500

    Update certbot module

--- docroot/modules/custom/certbot_challenge/src/Controller/CertbotChallengeController.php
+++ docroot/modules/custom/certbot_challenge/src/Controller/CertbotChallengeController.php
@@ -7,11 +7,10 @@
 namespace Drupal\certbot_challenge\Controller;
  
 use Drupal\Core\Controller\ControllerBase;
- 
+use Symfony\Component\HttpFoundation\Response;
+
 class CertbotChallengeController extends ControllerBase {
   public function content() {
-
-    use Symfony\Component\HttpFoundation\Response;
 
     $response = new Response();
     $response->setContent('_rCvQ8oDkW2S5q3sE_-u1bk9idn6CrPL35qjSb7jka4.2qn6s5gHRMlaCEKxm2wZeAFHequoOLhtJdQ7qyDOdVI');
