c07d0e18e181237e81a2a633edc34337d69bf574
Gregg Shanefelt <gshanefelt@beyondspotsanddots.com>
2018-01-12 12:02:20 -0500

    PHPCS validation fix

--- docroot/modules/custom/certbot_challenge/src/Controller/CertbotChallengeController.php
+++ docroot/modules/custom/certbot_challenge/src/Controller/CertbotChallengeController.php
@@ -1,20 +1,23 @@
 <?php
-/**
- * @file
- * Contains \Drupal\first_module\Controller\FirstController.
- */
- 
+
 namespace Drupal\certbot_challenge\Controller;
- 
+
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Comment.
+ */
 class CertbotChallengeController extends ControllerBase {
+
+  /**
+   * Comment.
+   */
   public function content() {
 
     $response = new Response();
     $response->setContent('_rCvQ8oDkW2S5q3sE_-u1bk9idn6CrPL35qjSb7jka4.2qn6s5gHRMlaCEKxm2wZeAFHequoOLhtJdQ7qyDOdVI');
     return $response;
   }
-}
 
+}
